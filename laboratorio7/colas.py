b1135084342465a2c624a047cbee4f5e30bdc3bb
Masterjaub527 <juan.gguarin@mail.escuelaing.edu.co>
2026-03-05 13:22:21 +0000

    Agrege librerias

---
+++ laboratorio7/colas.py
@@ -0,0 +1,44 @@
+class Nodo:
+    def __init__(self, item):
+        self.dato = item
+        self.siguiente = None
+        
+class Cola:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def is_empty(self):
+        return self.head == None
+    
+    def enqueue(self, item):
+        nuevo_nodo = Nodo(item)
+
+        if self.is_empty():
+            self.head = nuevo_nodo
+            self.tail = nuevo_nodo
+        else:
+            self.tail.siguiente = nuevo_nodo
+            self.tail = nuevo_nodo
+        
+    def dequeue(self):
+        if not self.is_empty():
+            dato = self.head.dato
+            self.head = self.head.siguiente
+            return dato
+        
+    def head(self):
+        if not self.is_empty():
+            return self.head.dato
+        
+    def tail(self):
+        if not self.is_empty():
+            return self.tail.dato
+        
+    def length(self):
+        contador = 0
+        nodo_actual = self.head
+        while nodo_actual is not None:
+            contador += 1
+            nodo_actual = nodo_actual.siguiente
+        return contador
---
+++ laboratorio7/lab.py
@@ -0,0 +1,2 @@
+import pila
+import colas
